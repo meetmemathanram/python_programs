0e12f0c3f6e0325c2f8cbdec3dbfe0f3742a5012
Mathan Ram <meetmemathanram@gmail.com>
2017-12-21 13:04:34 +0530

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,10 @@
+def print_lyrics():
+    print 'iam a lumberjack,and iam okey'
+    print 'ill sleep all night and i work all the day'
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+       
+    
+repeat_lyrics()    

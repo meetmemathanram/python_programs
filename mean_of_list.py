2ad6e6ddb18d99087315710d72efce337746665a
Mathan Ram <meetmemathanram@gmail.com>
2018-01-09 11:36:12 +0530

    Add files via upload

---
+++ mean_of_list.py
@@ -0,0 +1,11 @@
+#2. Write a function to compute and return the arithmentic mean for the values
+#contained in the given list.
+def mean(oflist):
+    sumofmean=(sum(oflist)/len(oflist))
+    print(sumofmean)
+    
+#oflist= int(input().split(sep=","))
+oflist= input().split()
+int(oflist)
+    
+mean(oflist)
